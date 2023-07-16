2ed481db32c5589cf78fcc1e898c4c3a134ee2d5
Christopher Ta <39348633+Digital1O1@users.noreply.github.com>
2023-07-15 19:44:50 -0500

    Update jetson_nano_start_up.sh

--- jetson_nano_start_up.sh
+++ jetson_nano_start_up.sh
@@ -9,17 +9,8 @@ sudo apt install gcc-8 g++-8
 sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-8 80 --slave /usr/bin/g++ g++ /usr/bin/g++-8
 sudo update-alternatives --config gcc
 gcc --version
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
-
-
-#echo "------------------- [ INSTALLING CUDA 11.2 ] -------------------"
-#wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/arm64/cuda-ubuntu2004.pin
-#sudo mv cuda-ubuntu2004.pin /etc/apt/preferences.d/cuda-repository-pin-600
-#wget https://developer.download.nvidia.com/compute/cuda/12.2.0/local_installers/cuda-tegra-repo-ubuntu2004-12-2-local_12.2.0-1_arm64.deb
-#sudo dpkg -i cuda-tegra-repo-ubuntu2004-12-2-local_12.2.0-1_arm64.deb
-#sudo cp /var/cuda-tegra-repo-ubuntu2004-12-2-local/cuda-*-keyring.gpg /usr/share/keyrings/
-#sudo apt-get -y update
-#sudo apt-get -y install cuda
 
 echo "------------------- [ INSTALLING NANO ] -------------------"
 sudo apt install -y nano
