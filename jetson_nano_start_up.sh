8b8fb6da0c465ce53c87e87426cebcee1cd4e06a
Christopher Ta <39348633+Digital1O1@users.noreply.github.com>
2023-11-16 16:44:52 -0600

    Update jetson_nano_start_up.sh

--- jetson_nano_start_up.sh
+++ jetson_nano_start_up.sh
@@ -5,7 +5,7 @@ echo "------------------- [ UPDATING JETSON NANO ] -------------------"
 sudo apt update -y && sudo apt upgrade -y
 
 echo "------------------- [ INSTALLING GCC V8 FOR OPENCV ] -------------------"
-sudo apt install gcc-8 g++-8
+sudo apt install -y gcc-8 g++-8
 sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-8 80 --slave /usr/bin/g++ g++ /usr/bin/g++-8
 sudo update-alternatives --config gcc
 gcc --version
@@ -22,14 +22,10 @@ sudo apt install -y python3-pip
 sudo pip3 install -U jetson-stats
 
 echo "------------------- [ BUILDING VIM FROM SOURCE ] -------------------"
-
-# Building VIM from source
-# Reference link: https://solarianprogrammer.com/2016/09/24/raspberry-pi-raspbian-building-installing-vim-8/
 cd ~
-sudo apt-get install ncurses-dev
+sudo apt-get install -y ncurses-dev
 git clone https://github.com/vim/vim.git vim-master
-cd vim-master
-cd src
+cd vim-master/src
 ./configure
 make -j 4
 sudo make install
@@ -151,33 +147,33 @@ sudo apt install -y python3-pip
 sudo apt install -y build-essential libssl-dev libffi-dev python3-dev
 sudo apt install -y curl
 
-# Change myUserName to whatever username you're using
-sudo adduser ctnano ssl-cert
-sudo apt install -y xfce4
-sudo chmod 777 /etc/xrdp/startwm.sh
-sudo apt-get install -y xfce4-terminal
-sudo update-alternatives --config x-terminal-emulator
-echo "Use the following command: sudo nano /etc/xrdp/startwm.sh"
-echo "Comment out the last two lines"
-echo "Add 'startxfce4' at the bottom of the file"
-echo "Then copy/paste in the terminal :  sudo service xrdp restart"
+# # Change myUserName to whatever username you're using
+# sudo adduser ctnano ssl-cert
+# sudo apt install -y xfce4
+# sudo chmod 777 /etc/xrdp/startwm.sh
+# sudo apt-get install -y xfce4-terminal
+# sudo update-alternatives --config x-terminal-emulator
+# echo "Use the following command: sudo nano /etc/xrdp/startwm.sh"
+# echo "Comment out the last two lines"
+# echo "Add 'startxfce4' at the bottom of the file"
+# echo "Then copy/paste in the terminal :  sudo service xrdp restart"
 
-echo "------------------- [ ENABLE VNC/REMOTE DESKTOP ] -------------------"
-echo "Reference link : https://raspberry-valley.azurewebsites.net/NVIDIA-Jetson-Nano/"
-echo "Use the following command: sudo nano /usr/share/glib-2.0/schemas/org.gnome.Vino.gschema.xml"
-echo "Add this after the first 'entry'"
-echo "<key name='enabled' type='b'>
-   <summary>Enable remote access to the desktop</summary>
-   <description>
-   If true, allows remote access to the desktop via the RFB
-   protocol. Users on remote machines may then connect to the
-   desktop using a VNC viewer.
-   </description>
-   <default>false</default>
-</key>"
-echo "Then : sudo glib-compile-schemas /usr/share/glib-2.0/schemas"
-echo "gsettings set org.gnome.Vino require-encryption false"
-echo "gsettings set org.gnome.Vino prompt-enabled false"
+# echo "------------------- [ ENABLE VNC/REMOTE DESKTOP ] -------------------"
+# echo "Reference link : https://raspberry-valley.azurewebsites.net/NVIDIA-Jetson-Nano/"
+# echo "Use the following command: sudo nano /usr/share/glib-2.0/schemas/org.gnome.Vino.gschema.xml"
+# echo "Add this after the first 'entry'"
+# echo "<key name='enabled' type='b'>
+#    <summary>Enable remote access to the desktop</summary>
+#    <description>
+#    If true, allows remote access to the desktop via the RFB
+#    protocol. Users on remote machines may then connect to the
+#    desktop using a VNC viewer.
+#    </description>
+#    <default>false</default>
+# </key>"
+# echo "Then : sudo glib-compile-schemas /usr/share/glib-2.0/schemas"
+# echo "gsettings set org.gnome.Vino require-encryption false"
+# echo "gsettings set org.gnome.Vino prompt-enabled false"
 sudo reboot now
 
 
