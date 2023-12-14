d6481712d3ac30993a9279dcddf644be7bd7eae6
Christopher Ta <39348633+Digital1O1@users.noreply.github.com>
2023-12-13 22:38:57 -0600

    Update and rename jetson_nano_start_up_OLD.sh to jetson_nano_start_up.sh

---
+++ jetson_nano_start_up.sh
@@ -0,0 +1,260 @@
+#! /bin/bash
+echo "Starting initial setup for Jetson Nano"
+
+echo "------------------- [ UPDATING JETSON NANO ] -------------------"
+sudo apt-get -y update  && sudo apt-get -y upgrade 
+
+# echo "------------------- [ INSTALLING GCC V8 FOR OPENCV ] -------------------"
+
+sudo apt install -y gcc-8 g++-8
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-8 80 --slave /usr/bin/g++ g++ /usr/bin/g++-8
+sudo update-alternatives --config gcc
+gcc --version
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+cd ~/.vim/bundle/YouCompleteMe
+python3 install.py --all
+
+echo "------------------- [ INSTALLING NANO ] -------------------"
+sudo apt install -y nano
+
+echo "------------------- [ INSTALLING JETSON STATS ] -------------------"
+
+sudo apt install -y python3-pip
+sudo pip3 install -U jetson-stats
+
+echo "------------------- [ BUILDING VIM FROM SOURCE ] -------------------"
+cd ~
+sudo apt-get install -y ncurses-dev
+git clone https://github.com/vim/vim.git vim-master
+cd vim-master/src
+./configure
+make -j 4
+sudo make install
+
+echo "------------------- [ INSTALLING XRDP / XFCE4 ] -------------------"
+# Reference link : https://raspberry-valley.azurewebsites.net/NVIDIA-Jetson-Nano/
+
+# XRDP / XFCE4
+sudo apt install -y xrdp
+sudo apt install -y python3-pip
+sudo apt install -y build-essential libssl-dev libffi-dev python3-dev
+sudo apt install -y curl
+echo "------------------- [ INSTALLING TIGHTVNC ] -------------------"
+sudo apt-get install -y xfce4 xfce4-goodies
+sudo apt-get install -y tightvncserver
+
+# Start TightVNC server to set up initial configuration
+tightvncserver
+
+# Stop TightVNC server
+tightvncserver -kill :1
+
+# Configure TightVNC server to start on boot
+echo "#!/bin/sh
+### BEGIN INIT INFO
+# Provides:          tightvncserver
+# Required-Start:    $local_fs $remote_fs $network
+# Required-Stop:     $local_fs $remote_fs $network
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+# Short-Description: Start TightVNC server at boot time
+# Description:       Start TightVNC server at boot time.
+### END INIT INFO
+
+# /etc/init.d/tightvncserver
+tightvncserver -geometry 1920x1080 -depth 24 :1
+" | sudo tee -a /etc/init.d/tightvncserver
+
+# Set executable permissions for the init script
+sudo chmod +x /etc/init.d/tightvncserver
+
+# Register the init script to run on boot
+sudo update-rc.d tightvncserver defaults
+
+# Start TightVNC server
+sudo /etc/init.d/tightvncserver start
+
+echo "TightVNC server installed and configured. You can now connect to your Jetson Nano using a VNC viewer on the IP address followed by :1 (e.g., 192.168.1.100:1)"
+
+echo "------------------- [ ENABLE VNC/REMOTE DESKTOP ] -------------------"
+echo "Reference link: https://raspberry-valley.azurewebsites.net/NVIDIA-Jetson-Nano/"
+echo "Use the following command: sudo nano /usr/share/glib-2.0/schemas/org.gnome.Vino.gschema.xml"
+echo "Add this after the first 'entry'"
+echo "<key name='enabled' type='b'>
+   <summary>Enable remote access to the desktop</summary>
+   <description>
+   If true, allows remote access to the desktop via the RFB
+   protocol. Users on remote machines may then connect to the
+   desktop using a VNC viewer.
+   </description>
+   <default>false</default>
+</key>"
+echo "Then: sudo glib-compile-schemas /usr/share/glib-2.0/schemas"
+echo "gsettings set org.gnome.Vino require-encryption false"
+echo "gsettings set org.gnome.Vino prompt-enabled false"
+
+echo "------------------- [ INSTALLING OPENCV WITH CUDA ] -------------------"
+
+#!/bin/bash
+set -e
+install_opencv () {
+  # Check if the file /proc/device-tree/model exists
+  if [ -e "/proc/device-tree/model" ]; then
+      # Read the model information from /proc/device-tree/model and remove null bytes
+      model=$(tr -d '\0' < /proc/device-tree/model)
+      # Check if the model information contains "Jetson Nano Orion"
+      echo ""
+      if [[ $model == *"Orin Nano"* ]]; then
+          echo "Detecting a Jetson Nano Orin."
+	  # Use always "-j 4"
+          NO_JOB=4
+          ARCH=8.7
+          PTX="sm_87"
+      elif [[ $model == *"Jetson Nano"* ]]; then
+          echo "Detecting a regular Jetson Nano."
+          ARCH=5.3
+          PTX="sm_53"
+	  # Use "-j 4" only swap space is larger than 5.5GB
+	  FREE_MEM="$(free -m | awk '/^Swap/ {print $2}')"
+	  if [[ "FREE_MEM" -gt "5500" ]]; then
+	    NO_JOB=4
+	  else
+	    echo "Due to limited swap, make only uses 1 core"
+	    NO_JOB=1
+	  fi
+      else
+          echo "Unable to determine the Jetson Nano model."
+          exit 1
+      fi
+      echo ""
+  else
+      echo "Error: /proc/device-tree/model not found. Are you sure this is a Jetson Nano?"
+      exit 1
+  fi
+  
+  echo "Installing OpenCV 4.8.0 on your Nano"
+  echo "It will take 3.5 hours !"
+  
+  # reveal the CUDA location
+  cd ~
+  sudo sh -c "echo '/usr/local/cuda/lib64' >> /etc/ld.so.conf.d/nvidia-tegra.conf"
+  sudo ldconfig
+  
+  # install the Jetson Nano dependencies first
+  if [[ $model == *"Jetson Nano"* ]]; then
+    sudo apt-get install -y build-essential git unzip pkg-config zlib1g-dev
+    sudo apt-get install -y python3-dev python3-numpy
+    sudo apt-get install -y python-dev python-numpy python-pip
+    sudo apt-get install -y gstreamer1.0-tools libgstreamer-plugins-base1.0-dev
+    sudo apt-get install -y libgstreamer-plugins-good1.0-dev
+    sudo apt-get install -y libtbb2 libgtk-3-dev v4l2ucp libxine2-dev
+  fi
+  # install the common dependencies
+  sudo apt-get install -y cmake
+  sudo apt-get install -y libjpeg-dev libjpeg8-dev libjpeg-turbo8-dev
+  sudo apt-get install -y libpng-dev libtiff-dev libglew-dev
+  sudo apt-get install -y libavcodec-dev libavformat-dev libswscale-dev
+  sudo apt-get install -y libgtk2.0-dev libgtk-3-dev libcanberra-gtk*
+  sudo apt-get install -y python3-pip
+  sudo apt-get install -y libxvidcore-dev libx264-dev
+  sudo apt-get install -y libtbb-dev libdc1394-22-dev libxine2-dev
+  sudo apt-get install -y libv4l-dev v4l-utils qv4l2
+  sudo apt-get install -y libtesseract-dev libpostproc-dev
+  sudo apt-get install -y libavresample-dev libvorbis-dev
+  sudo apt-get install -y libfaac-dev libmp3lame-dev libtheora-dev
+  sudo apt-get install -y libopencore-amrnb-dev libopencore-amrwb-dev
+  sudo apt-get install -y libopenblas-dev libatlas-base-dev libblas-dev
+  sudo apt-get install -y liblapack-dev liblapacke-dev libeigen3-dev gfortran
+  sudo apt-get install -y libhdf5-dev libprotobuf-dev protobuf-compiler
+  sudo apt-get install -y libgoogle-glog-dev libgflags-dev
+ 
+  # remove old versions or previous builds
+  cd ~ 
+  sudo rm -rf opencv*
+  # download the latest version
+  git clone --depth=1 https://github.com/opencv/opencv.git
+  git clone --depth=1 https://github.com/opencv/opencv_contrib.git
+  
+  # set install dir
+  cd ~/opencv
+  mkdir build
+  cd build
+  
+  # run cmake
+  cmake -D CMAKE_BUILD_TYPE=RELEASE \
+  -D CMAKE_INSTALL_PREFIX=/usr \
+  -D OPENCV_EXTRA_MODULES_PATH=~/opencv_contrib/modules \
+  -D EIGEN_INCLUDE_PATH=/usr/include/eigen3 \
+  -D WITH_OPENCL=OFF \
+  -D CUDA_ARCH_BIN=${ARCH} \
+  -D CUDA_ARCH_PTX=${PTX} \
+  -D WITH_CUDA=ON \
+  -D WITH_CUDNN=ON \
+  -D WITH_CUBLAS=ON \
+  -D ENABLE_FAST_MATH=ON \
+  -D CUDA_FAST_MATH=ON \
+  -D OPENCV_DNN_CUDA=ON \
+  -D ENABLE_NEON=ON \
+  -D WITH_QT=OFF \
+  -D WITH_OPENMP=ON \
+  -D BUILD_TIFF=ON \
+  -D WITH_FFMPEG=ON \
+  -D WITH_GSTREAMER=ON \
+  -D WITH_TBB=ON \
+  -D BUILD_TBB=ON \
+  -D BUILD_TESTS=OFF \
+  -D WITH_EIGEN=ON \
+  -D WITH_V4L=ON \
+  -D WITH_LIBV4L=ON \
+  -D WITH_PROTOBUF=ON \
+  -D OPENCV_ENABLE_NONFREE=ON \
+  -D INSTALL_C_EXAMPLES=ON \
+  -D INSTALL_PYTHON_EXAMPLES=ON \
+  -D PYTHON3_PACKAGES_PATH=/usr/lib/python3/dist-packages \
+  -D OPENCV_GENERATE_PKGCONFIG=ON \
+  -D BUILD_EXAMPLES=ON ..
+ 
+  make -j ${NO_JOB} 
+  
+  directory="/usr/include/opencv4/opencv2"
+  if [ -d "$directory" ]; then
+    # Directory exists, so delete it
+    sudo rm -rf "$directory"
+  fi
+  
+  sudo make install
+  sudo ldconfig
+  
+  # cleaning (frees 320 MB)
+  make clean
+  sudo apt-get update
+  
+  echo "Congratulations!"
+  echo "You've successfully installed OpenCV 4.8.0 on your Nano"
+}
+
+cd ~
+
+if [ -d ~/opencv/build ]; then
+  echo " "
+  echo "You have a directory ~/opencv/build on your disk."
+  echo "Continuing the installation will replace this folder."
+  echo " "
+  
+  printf "Do you wish to continue (Y/n)?"
+  read answer
+
+  if [ "$answer" != "${answer#[Nn]}" ] ;then 
+      echo "Leaving without installing OpenCV"
+  else
+      install_opencv
+  fi
+else
+    install_opencv
+fi
+
+
+
+sudo reboot now
+
+
